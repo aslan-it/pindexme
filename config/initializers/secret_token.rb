5b596b3db87ccbcbdbbd0307e8bf55062a366411
Sezai Aslan <aslan.sezai@gmail.com>
2013-10-13 16:19:20 +0200

    teset

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pindexme::Application.config.secret_key_base = '34d2c6f9a6ffe0a1ff69a521bd7dcf97cee4f13555a948ab2ce630154c54c4b9d50c5fc8dd0538436a86492ea4c58967017689273addaaa79d1ce9066f4b0794'
